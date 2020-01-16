4a562685b4cfa169ae81be264885c67484e6fbe7
Alipio Neto <alipio.neto@transdatasmart.com.br>
2020-01-16 09:41:38 -0300

    Separando em uma classe

---
+++ SearchjsToExpression/ExtractJToken.cs
@@ -0,0 +1,169 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SearchjsToExpression
+{
+    public static class ExtractJToken
+    {
+        private static eOrderTypes mainOrder = eOrderTypes.Null;
+        private static eOrderTypes currentOrder = eOrderTypes.And;
+        private static bool mainNot = false;
+        private static bool currentNot = false;
+        private static string comparator = "";
+
+        private static void SetModifiers( JProperty prop, bool setMain )
+        {
+            if( prop.Name == "_join" )
+            {
+                if( prop.Value.ToString( ) == "OR" )
+                    currentOrder = eOrderTypes.Or;
+                else
+                    currentOrder = eOrderTypes.And;
+
+                if( mainOrder == eOrderTypes.Null && setMain )
+                    mainOrder = currentOrder;
+            }
+            else if( prop.Name == "_not" )
+            {
+                if( ( bool ) prop.Value )
+                {
+                    if( setMain )
+                        mainNot = !mainNot;
+                    else
+                        currentNot = !currentNot;
+                }
+            }
+            else
+            {
+                if( ( bool ) prop.Value )
+                    comparator = prop.Name;
+            }
+        }
+
+        private static Expression<Func<T, bool>> GetFromObject<T>( JProperty prop )
+        {
+            //{ \"Detail.age\" : { \"from\" : 30, \"to\": 35 } }
+            var auxList = new List<Expression<Func<T, bool>>>( );
+            foreach( JProperty child in prop.Value.Children<JProperty>( ) )
+            {
+                auxList.Add( Utils.CreateExpression<T>( prop.Name, child.Value, false, child.Name ) );
+            }
+
+            return Utils.BuildAnd( mainNot, auxList.ToArray( ) );
+        }
+
+        private static Expression<Func<T, bool>> ProcessTerms<T>( JToken item )
+        {
+            var auxList = new List<Expression<Func<T, bool>>>( );
+
+            currentOrder = eOrderTypes.And;
+            currentNot = mainNot;
+            comparator = "";
+
+            foreach( JProperty child in item.Children<JProperty>( ).OrderBy( x => x.Name ) )
+            {
+                if( child.Name.StartsWith( "_" ) )
+                {
+                    SetModifiers( child, false );
+                }
+                else
+                {
+                    if( child.Value.Type == JTokenType.Object )
+                    {
+                        auxList.Add( GetFromObject<T>( child ) );
+                    }
+                    else
+                    {
+                        auxList.Add( Utils.CreateExpression<T>( child.Name, child.Value, currentNot, comparator ) );
+                    }
+                }
+            }
+
+            return ( currentOrder == eOrderTypes.And )
+                        ? Utils.BuildAnd( auxList.ToArray( ) )
+                        : Utils.BuildOrElse( auxList.ToArray( ) );
+        }
+
+        private static List<Expression<Func<T, bool>>> GetFromArray<T>( JProperty prop )
+        {
+            var ret = new List<Expression<Func<T, bool>>>( );
+            var auxList = new List<Expression<Func<T, bool>>>( );
+
+            foreach( var item in ( JArray ) prop.Value )
+            {
+                if( item.Type == JTokenType.Object ) //terms
+                {
+                    ret.Add( ProcessTerms<T>( item ) );
+                }
+                else
+                {
+                    auxList.Add( Utils.CreateExpression<T>( prop.Name, item, mainNot, comparator ) );
+                }
+            }
+
+            if( auxList.Count > 0 ) //prop.Name != "terms"
+            {
+                //{ \"name\":[ \"John\", \"Joana\" ]}
+                ret.Add( Utils.BuildOrElse( auxList.ToArray( ) ) );
+            }
+
+            return ret;
+        }
+
+        public static Expression<Func<T, bool>> Extract<T>( JToken node )
+        {
+            List<Expression<Func<T, bool>>> list = new List<Expression<Func<T, bool>>>( );
+            list = ProcessJToken<T>( node );
+
+            if( list.Count == 1 )
+            {
+                return list.FirstOrDefault( );
+            }
+            else
+            {
+                if( mainOrder == eOrderTypes.And || mainOrder == eOrderTypes.Null )
+                {
+                    return Utils.BuildAnd( list.ToArray( ) );
+                }
+                else
+                {
+                    return Utils.BuildOrElse( list.ToArray( ) );
+                }
+            }
+        }
+
+        private static List<Expression<Func<T, bool>>> ProcessJToken<T>( JToken node )
+        {
+            List<Expression<Func<T, bool>>> list = new List<Expression<Func<T, bool>>>( );
+
+            foreach( JProperty prop in node.Children<JProperty>( ).OrderBy( x => x.Name ) )
+            {
+                if( prop.Value.Type == JTokenType.Object )
+                {
+                    list.Add( GetFromObject<T>( prop ) );
+                }
+                else if( prop.Value.Type == JTokenType.Array )
+                {
+                    list.AddRange( GetFromArray<T>( prop ) );
+                }
+                else
+                {
+                    if( prop.Name.StartsWith( "_" ) )
+                    {
+                        SetModifiers( prop, true );
+                    }
+                    else
+                    {
+                        list.Add( Utils.CreateExpression<T>( prop.Name, prop.Value, mainNot, comparator ) );
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
--- SearchjsToExpression/Program.cs
+++ SearchjsToExpression/Program.cs
@@ -47,12 +47,6 @@ namespace SearchjsToExpression
     {
         static void Main( string[ ] args )
         {
-            eOrderTypes mainOrder = eOrderTypes.Null;
-            eOrderTypes currentOrder = eOrderTypes.And;
-            bool mainNot = false;
-            bool currentNot = false;
-            string comparator = "";
-
             var people = new List<Person>( )
             {
                 new Person( ){ 
@@ -98,13 +92,13 @@ namespace SearchjsToExpression
             //string json = "{ \"_not\": true, \"name\":[ \"John\", \"Joana\" ]}";
             //string json = "{ \"name\":\"Joan\", \"_start\": true }";
             //string json = "{ \"name\":\"de\", \"_word\": true }";
-            string json = "{ \"name\":\"de\", \"_word\": false }";
+            //string json = "{ \"name\":\"de\", \"_word\": false }";
             //string json = "{ \"detail.dogs.race\": \"H2\" }";
             //string json = "{ \"Detail.age\" : { \"from\" : 30, \"to\": 35 } }";
             //string json = "{ \"_not\": true, \"Detail.age\" : { \"from\" : 30, \"to\": 35 } }";
             //string json = "{ \"name\": \"Joana\",\"detail.age\": 30,\"_join\": \"OR\"}";
             //string json = " { \"name\": \"Joana\", \"Detail.age\" : { \"from\" : 25 , \"to\" : 40 }, \"_join\": \"OR\" }";
-            //string json = "{ \"terms\" :[ { \"name\": \"Joana\", \"Detail.age\": 30},{ \"name\": \"Jill\",\"Detail.age\": 18 } ], \"_join\": \"OR\" }";
+            string json = "{ \"terms\" :[ { \"name\": \"Joana\", \"Detail.age\": 30},{ \"name\": \"Jill\",\"Detail.age\": 18 } ], \"_join\": \"OR\" }";
             //string json = "{ \"terms\" :[ { \"name\": \"Joana\", \"Detail.age\": 30, \"_join\": \"OR\"},{ \"name\": \"Jill\",\"Detail.age\": 18, \"_join\": \"OR\" } ], \"_join\": \"AND\" }";
             //string json = "{ \"terms\" :[ { \"name\": \"Joana\", \"Detail.age\": 30, \"_not\": true } ], \"_not\": true }";
             //string json = "{ \"terms\" :[ { \"name\": \"Joana\", \"Detail.age\": 30} ], \"_not\": true }";
@@ -116,162 +110,8 @@ namespace SearchjsToExpression
             //string json = "{ \"terms\" :[ { \"name\":\"Lara\", \"_text\": true }, { \"name\": \"Joana\" } ]}";
             //string json = "{ \"_text\": true, \"terms\" :[ { \"name\":\"Lara\" }, { \"name\": \"Joana\" } ]}";
 
-            Expression<Func<Person, bool>> exp = null;
-            List<Expression<Func<Person, bool>>> list = new List<Expression<Func<Person, bool>>>( );
             JToken node = JToken.Parse( json );
-
-            foreach( JProperty prop in node.Children<JProperty>( ).OrderBy( x => x.Name ) )
-            {
-                if( prop.Value.Type == JTokenType.Object )
-                {
-                    //{ \"Detail.age\" : { \"from\" : 30, \"to\": 35 } }
-                    var auxList = new List<Expression<Func<Person, bool>>>( );
-                    foreach( JProperty child in prop.Value.Children<JProperty>( ) )
-                    {
-                        auxList.Add( Utils.CreateExpression<Person>( prop.Name, child.Value, false, child.Name ) );
-                    }
-
-                    list.Add( Utils.BuildAnd( mainNot, auxList.ToArray( ) ) );
-                }
-                else if( prop.Value.Type == JTokenType.Array )
-                {
-                    var auxList = new List<Expression<Func<Person, bool>>>( );
-                    foreach( var item in ( JArray ) prop.Value )
-                    {
-                        if( item.Type == JTokenType.Object ) //terms
-                        {
-                            currentOrder = eOrderTypes.And;
-                            currentNot = mainNot;
-                            comparator = "";
-
-                            foreach( JProperty child in item.Children<JProperty>( ).OrderBy( x => x.Name ) )
-                            {
-                                if( child.Name.StartsWith( "_" ) )
-                                { 
-                                    if( child.Name == "_join" )
-                                    {
-                                        if( child.Value.ToString( ) == "OR" )
-                                            currentOrder = eOrderTypes.Or;
-                                    } 
-                                    else if( child.Name == "_not" )
-                                    {
-                                        if( ( bool ) child.Value )
-                                            currentNot = !currentNot;
-                                    }
-                                    else
-                                    {
-                                        if( ( bool ) child.Value )
-                                            comparator = child.Name;
-                                    }
-                                }
-                                else
-                                {
-                                    if( child.Value.Type == JTokenType.Object )
-                                    {
-                                        //{ \"Detail.age\" : { \"from\" : 30, \"to\": 35 } }
-                                        var newAuxList = new List<Expression<Func<Person, bool>>>( );
-                                        foreach( JProperty ch in child.Value.Children<JProperty>( ) )
-                                        {
-                                            newAuxList.Add( Utils.CreateExpression<Person>( child.Name, ch.Value, false, ch.Name ) );
-                                        }
-
-                                        auxList.Add( Utils.BuildAnd( currentNot, newAuxList.ToArray( ) ) );
-                                    }
-                                    else
-                                    {
-                                        auxList.Add( Utils.CreateExpression<Person>( child.Name, child.Value, currentNot, comparator ) );
-                                    }
-                                }
-                            }
-
-                            list.Add( currentOrder == eOrderTypes.And ?  Utils.BuildAnd( auxList.ToArray( ) ) 
-                                : Utils.BuildOrElse( auxList.ToArray( ) ) );
-                            auxList = new List<Expression<Func<Person, bool>>>( );
-                        }
-                        else
-                        {
-                            auxList.Add( Utils.CreateExpression<Person>( prop.Name, item, mainNot, comparator ) );
-                        }
-                    }
-
-                    if( prop.Name != "terms" )
-                    {
-                        //{ \"name\":[ \"John\", \"Joana\" ]}
-                        list.Add( Utils.BuildOrElse( auxList.ToArray( ) ) );
-                    }
-                }
-                else
-                {
-                    if( prop.Name.StartsWith( "_" ) )
-                    {
-                        if( prop.Name == "_join" )
-                        {
-                            if( prop.Value.ToString( ) == "OR" )
-                                currentOrder = eOrderTypes.Or;
-                            else
-                                currentOrder = eOrderTypes.And;
-
-                            if (mainOrder == eOrderTypes.Null)
-                                mainOrder = currentOrder;
-                        }
-                        else if( prop.Name == "_not" )
-                        {
-                            if( ( bool ) prop.Value )
-                                mainNot = !mainNot;
-                        }
-                        else
-                        {
-                            if( ( bool ) prop.Value )
-                                comparator = prop.Name;
-                        }
-                    }
-                    else
-                    {
-                        list.Add( Utils.CreateExpression<Person>( prop.Name, prop.Value, mainNot, comparator ) );
-                    }
-                }
-            }
-
-            if( list.Count == 1 )
-            {
-                exp = list.FirstOrDefault( );
-            } 
-            else
-            {
-                if( mainOrder == eOrderTypes.And )
-                {
-                    exp = Utils.BuildAnd( list.ToArray( ) );
-                } 
-                else
-                {
-                    exp = Utils.BuildOrElse( list.ToArray( ) );
-                }
-            }
-
-            #region coments
-            //List<Expression<Func<Person, bool>>> list = new List<Expression<Func<Person, bool>>>( );
-            //list.Add( Utils.CreateExpression<Person>( "Detail.Gender", "M" ) );
-            //list.Add( Utils.CreateExpression<Person>( "Detail.Gender", "F" ) );
-            //list.Add( Utils.CreateExpression<Person>( "Detail.Gender", "B" ) );
-            //list.Add( Utils.CreateExpression<Person>( "Name", "João") );
-
-            //exp = Utils.BuildOrElse( list.ToArray( ) );
-            //var exp = Utils.BuildAnd( list.ToArray( ) );
-            //var exp2 = Utils.BuildOrElse( exp, Utils.CreateExpression<Person>( "Detail.Gender", "F" ) );
-
-            //var exp = Utils.CreateExpression<Person>( "Cars.Brand", "BMW", "", true );
-            //var exp = Utils.CreateExpression<Person>( "Detail.Dogs.Name", "Max" );
-            //var exp = Utils.CreateExpression<Person>( "Detail.Dogs.Race", "H2" );
-            //var exp = Utils.CreateExpression<Person>( "Cares.Brand", "BMW" );
-            //var exp = Utils.CreateExpression<Person>( "Lotery", 1 );
-            //var exp = Utils.CreateExpression<Person>( "Cars.HP", 400, "lte" );
-            //var exp = Utils.CreateExpression<Person>( "Detail.Doges.Name", "Max" );
-            //var exp = Utils.CreateExpression<Person>( "Detail.Days", 2 );
-            //var exp = Utils.CreateExpression<Person>( "Name", "edr", "_text", true );
-            //var exp = Utils.CreateExpression<Person>( "Name", "de", "_word");
-            //var exp = Utils.CreateExpression<Person>( "cars.brand", "BMW" );
-            //var exp = Utils.CreateExpression<Person>( "detail.dogs.race", "H2" );
-            #endregion
+            var exp = ExtractJToken.Extract<Person>( node );
 
             var result = people.Where( exp.Compile() );
 
